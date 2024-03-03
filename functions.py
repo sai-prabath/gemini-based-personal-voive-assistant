fc1e6598dadd064e50563a40429478c48890ff0c
sai prabath <saiprabathcse@gmail.com>
2024-03-03 15:50:27 +0530

    Mod:Package creation and functions seperation

---
+++ functions.py
@@ -0,0 +1,46 @@
+from __init__ import *
+
+# Speaks Query
+def say(query):
+    try:
+        os.system(f"say {query}")
+    except:
+        print("Escape sequence occured, cant readable")
+
+# Takes Voice Command
+def takecmd():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold= 1
+        audio=r.listen(source)
+        try:
+            query=r.recognize_google(audio,language="en - in")
+            print(f"user said: {query}")
+            return query
+        except Exception as e:
+            return "some Error occured , please check your internet and say again"
+
+# Opens a site
+def OpenSite(site):
+    try:
+        webbrowser.open(site)
+    except:
+        say("sorry Can.t open site")
+
+# Says the Time
+def TellTime():
+    time = datetime.datetime.now().strftime("%H:%M:%S")
+    say(f"The Time is {time}")
+
+# bard connect
+def tobard(chatbot,query,response,count):
+    try:
+        response = chatbot.ask(query)
+        return response['content']
+    except:
+        try:
+            response=response.reply(query)
+            return response.messages[count]['content']
+        except:
+            print("Sorry i can't assist you with that")
+            say("Sorry i can't assist you with that")
